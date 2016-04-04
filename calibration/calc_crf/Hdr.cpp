bed5250a629eda49a4ab27f0ba2c53bc0e623478
shuda <lishuda1980@gmail.com>
2016-04-04 17:59:05 +0100

    license added.

--- calibration/calc_crf/Hdr.cpp
+++ calibration/calc_crf/Hdr.cpp
@@ -1,3 +1,24 @@
+//Copyright(c) 2016 Shuda Li[lishuda1980@gmail.com]
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//FOR A PARTICULAR PURPOSE AND NON - INFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+//
+
 #include <opencv2/photo.hpp>
 #include "opencv2/imgcodecs.hpp"
 #include <opencv2/highgui.hpp>
@@ -7,6 +28,7 @@
 #include <fstream>
 using namespace cv;
 using namespace std;
+string _path;
 
 struct SHist{
 	vector<Point_<short>> _vBXY[256];
@@ -193,7 +215,6 @@ void loadExposureSeq(vector<Mat>& images, vector<float>& times, vector<short>& I
 	return;
 }
 
-string _path;
 int main(int, char**argv)
 {
 	//update the location of the source images
